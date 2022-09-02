6c66cdbeafb91abf13645665ee22fb3e4bc2f9ce
HongJoonHyung <sesa0802@naver.com>
2022-09-02 12:43:53 +0900

    config : styled-reset 설치

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,13 +1,15 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-
+  ${reset};
   *, *::before, *::after {
     box-sizing: border-box;
   }
 
   body {
     font-family: "Helvetica", "Arial", sans-serif;
+    font-size:14px;
     line-height: 1.5;
   }
 `;
