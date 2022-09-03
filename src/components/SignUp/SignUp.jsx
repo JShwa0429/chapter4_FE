36df9648fff4695688f114ee7f6325071f8d46c1
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-09-03 23:05:04 +0900

    input 수정

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,33 +1,79 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
-
+import { useState } from 'react';
 const SignUp = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  // onChangeHandler
+  const onEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+   const onNameHandler = (event) => {
+    setNickName(event.target.value);
+  };
+   const onPasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+   const onPasswordConfirmHandler = (event) => {
+    setPasswordConfirm(event.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+  };
 
   return (
     <SignUpLayoutBox>
-      <SignUpDiv>
-        <SignUpTitle>Sign Up</SignUpTitle>
-        <SignUpSubTitle>이메일</SignUpSubTitle>
-        <SignUpInput
-          type="email"
-        ></SignUpInput>
-        <SignUpSubTitle>닉네임</SignUpSubTitle>
-        <SignUpInput
-          type="text"
-        ></SignUpInput>
-        <SignUpSubTitle
-          type="password"
-        >비밀번호</SignUpSubTitle>
-        <SignUpInput
-          type="password"
-        ></SignUpInput>
-        <SignUpSubTitle>비밀번호 확인</SignUpSubTitle>
-        <SignUpInput></SignUpInput>
-        <SignUpButton onClick={() => { navigate("/login"); }}>가입하기</SignUpButton>
-      </SignUpDiv>
-    </SignUpLayoutBox>
+      <form onSubmit={onSubmitHandler}>
+        <SignUpDiv>
+          <SignUpTitle>Sign Up</SignUpTitle>
+
+          <SignUpSubTitle>이메일</SignUpSubTitle>
+          <SignUpInput
+            type="email"
+            value={email}
+            onChange={onEmailHandler}
+            required
+
+          ></SignUpInput>
+
+          <SignUpSubTitle>닉네임</SignUpSubTitle>
+          <SignUpInput
+            type="text"
+            value={nickName}
+            onChange={onNameHandler}
+            required
+
+          ></SignUpInput>
+
+          <SignUpSubTitle>비밀번호</SignUpSubTitle>
+          <SignUpInput
+            type="password"
+            value={password}
+            onChange={onPasswordHandler}
+            required
+
+          ></SignUpInput>
+
+          <SignUpSubTitle>비밀번호 확인</SignUpSubTitle>
+          <SignUpInput
+            type="password"
+            value={passwordConfirm}
+            onChange={onPasswordConfirmHandler}
+            required
+
+          ></SignUpInput>
+
+          <SignUpButton onClick={() => { navigate("/login"); }}>가입하기</SignUpButton>
+          </SignUpDiv>
+        </form>
+      </SignUpLayoutBox>
   );
 };
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movie from "./redux/modules/CommentSlice"
+import login from "./redux/modules/loginSlice"
 
 const store = configureStore({
-  reducer: {movie},
+  reducer: {movie, login },
 });
 
 export default store;
