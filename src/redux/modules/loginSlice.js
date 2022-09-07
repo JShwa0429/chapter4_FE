dc0ce491c2ab4d4a00039dcb36cc2543a401bd30
Yoo Mina <110373500+maydec2298@users.noreply.github.com>
2022-09-08 00:17:11 +0900

    안쓰는 코드, 주석 정리

--- src/redux/modules/loginSlice.js
+++ src/redux/modules/loginSlice.js
@@ -1,10 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 
 // axios 기본 세팅
 import { api } from "../../shared/api";
 
-// // thunk 함수
 
 // 로그인
 export const __login = createAsyncThunk(
@@ -18,43 +16,16 @@ export const __login = createAsyncThunk(
   }
 );
 
-// export const __login = createAsyncThunk(
-//   "log/LOGIN_LOG",
-//   async (payload, thunkAPI) => {
-
-//     const { data } = await api.post("/api/user/login", payload)
-// 	localStorage.setItem("refresh-token", response.data.token);
-//     return thunkAPI.fulfillWithValue(data);
-    
-//   }
-// );
-// 재 접속시 토큰 유효기간 확인
-export const __checkToken = createAsyncThunk(
-  "__checkToken/CHECK_LOG",
-  async (payload, thunkAPI) => {
-    // 토큰으로 유효값 확인하기
-    const response = await api.get("/auth");
-    // 상태값 true / false
-    return response.data;
-  }
-);
-
-// // slice
-
+// slice
 const loginSlice = createSlice({
   name: "login",
   initialState: {
-    // 초기값, 유저 닉네임 로그인 상태
-    // 전체 loading, error 값 저장
     user: { nickName: "", result: false },
     loading: false,
     error: null,
   },
   reducers: {
-    // 로그 아웃시 상태값 초기화
-    logOutUser: (state, payload) => {
-      state.user = { nickName: "", result: false };
-    },
+  
   },
   //
   extraReducers: (builder) => {
@@ -74,20 +45,10 @@ const loginSlice = createSlice({
       .addCase(__login.pending, (state, action) => {
         state.loading = true;
       })
-      // 토큰 확인하기
-      .addCase(__checkToken.fulfilled, (state, action) => {
-        state.loading = false;
-        state.user = action.payload;
-      })
-      .addCase(__checkToken.rejected, (state, action) => {
-        state.loading = false;
-      })
-      .addCase(__checkToken.pending, (state, action) => {
-        state.loading = true;
-      });
+
   },
 });
 
-// // reducer dispatch하기 위해 export 하기
+
 export const { logOutUser } = loginSlice.actions;
 export default loginSlice.reducer;
--- src/redux/modules/signupSlice.js
+++ src/redux/modules/signupSlice.js
@@ -1,10 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 
 // axios 기본 세팅
 import { api } from "../../shared/api";
 
-// thunk 함수
 
 // 회원가입
 export const __signup = createAsyncThunk(
@@ -18,42 +16,9 @@ export const __signup = createAsyncThunk(
     return thunkAPI.fulfillWithValue(data);
   }
 );
-// export const __signup = createAsyncThunk(
-//   "signup/SIGNUP_LOG",
-//   async (payload, thunkAPI) => {
-//     const response = await api.post("/api/user/signup", payload);
 
-//     // 회원가입 성공 시 alert & 상태 저장
-//     alert("회원가입이 완료됐습니다.");
-//     return response.data.result;
-//   }
-// );
-// 아이디 중복확인
-export const __checkUsername = createAsyncThunk(
-  "signup/CHECKID_LOG",
-  async (payload, thunkAPI) => {
-    // const response = await axios.get(`user/email/${payload}`);
-    const response = await axios.get("api/user/signup/${payload}");
-console.log(response)
-    // 중복확인 결과에 따라 alert 후 상태 저장
-    if (!response.data.result) alert("동일한 아이디가 존재합니다");
-    return response.data.result;
-  }
-);
-
-// 닉네임 중복확인
-export const __checkNickname = createAsyncThunk(
-  "signup/CHECKNICK_LOG",
-  async (payload, thunkAPI) => {
-    const response = await api.get(`/user/nickName/${payload}`);
-    // 중복확인 결과에 따라 alert 후 상태 저장
-    if (!response.data.result) alert("동일한 닉네임이 존재합니다");
-    return response.data.result;
-  }
-);
 
 // // slice
-
 const signupSlice = createSlice({
   name: "signup",
   initialState: {
@@ -63,14 +28,7 @@ const signupSlice = createSlice({
     checkMsg: "",
   },
   reducers: {
-    // 아이디 중복확인 상태 변경 ( 중복 확인 후 아이디 변경 시 실행)
-    changeCheckName: (state, payload) => {
-      state.checkName = false;
-    },
-    // 닉네임 중복확인 상태 변경 ( 중복 확인 후 닉네임 변경 시 실행)
-    changeCheckNick: (state) => {
-      state.checkNick = false;
-    },
+
   },
 
   extraReducers: (builder) => {
@@ -81,31 +39,9 @@ const signupSlice = createSlice({
         state.success = action.payload;
       })
 
-      // 아이디 중복검사
-      .addCase(__checkUsername.fulfilled, (state, action) => {
-        // 중복확인 상태 저장
-        state.checkName = action.payload;
-      })
-      // 에러로 중복확인 값 넘어왔을 때
-      // 잘못된 방법인 것 같다........
-      .addCase(__checkUsername.rejected, (state, action) => {
-        state.checkName = true;
-      })
-
-      // 닉네임 중복검사
-      .addCase(__checkNickname.fulfilled, (state, action) => {
-        // 중복확인 상태 저장
-        state.checkNick = action.payload;
-      })
-      // 에러로 중복확인 값 넘어왔을 때
-      // 잘못된 방법인 것 같다........
-      .addCase(__checkNickname.rejected, (state, action) => {
-        state.checkNick = true;
-      });
   },
 });
 
 
-// // reducer dispatch하기 위해 export 하기
-export const { changeCheckName, changeCheckNick } = signupSlice.actions;
+export const { } = signupSlice.actions;
 export default signupSlice.reducer;
